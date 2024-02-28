87efc838ed86d7b8ce9eb9798c3d86a6244c3863
Dekun Ma <william@dekun.me>
2024-02-27 19:37:51 -0500

    Only enable yale.edu email on sign up

--- screens/EnterEmailScreen.jsx
+++ screens/EnterEmailScreen.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {
+  AlertCircleIcon,
   Box,
   Button,
   ButtonText,
   Center,
+  FormControl,
+  FormControlError,
+  FormControlErrorIcon,
+  FormControlErrorText,
+  FormControlHelper,
+  FormControlHelperText,
   Heading,
   Input,
   InputField,
@@ -15,8 +22,25 @@ import api from '../api';
 const EnterEmailScreen = ({navigation}) => {
   const [isButtonDisabled, setIsButtonDisabled] = React.useState(false);
   const [email, setEmail] = React.useState('');
+  const [isEmailInvalid, setIsEmailInvalid] = React.useState(false);
+  const [emailInvalidText, setEmailInvalidText] = React.useState('');
+
+  useEffect(() => {
+    if (email === '') {
+      setIsEmailInvalid(false);
+      setIsButtonDisabled(true);
+    } else {
+      setIsButtonDisabled(false);
+    }
+  }, [email]);
 
   const handlePressButton = () => {
+    if (!email.endsWith('@yale.edu')) {
+      setIsEmailInvalid(true);
+      setEmailInvalidText('Please use your yale.edu email.');
+      return;
+    }
+
     setIsButtonDisabled(true);
     api
       .post('/users/send-verification-code', {email: email})
@@ -39,17 +63,28 @@ const EnterEmailScreen = ({navigation}) => {
         <Heading size="xl" color="$primary500" mb="$4">
           Enter Your Email
         </Heading>
-        <Text color="$secondary300" textAlign="center" mb="$10">
+        <Text color="$secondary300" textAlign="center" mb="$4">
           We will send a verification code to your email
         </Text>
         <Box w="$full" mb="$10" p="$4">
-          <Input
-            variant="outline"
-            size="lg"
-            isInvalid={false}
-            isReadOnly={false}>
-            <InputField placeholder="Email" onChangeText={e => setEmail(e)} />
-          </Input>
+          <FormControl size="lg" isInvalid={isEmailInvalid} isReadOnly={false}>
+            <Input>
+              <InputField
+                type="email"
+                placeholder="Email"
+                onChangeText={e => setEmail(e)}
+              />
+            </Input>
+            <FormControlHelper>
+              <FormControlHelperText>
+                ⚠ Please use your yale.edu email at this time
+              </FormControlHelperText>
+            </FormControlHelper>
+            <FormControlError>
+              <FormControlErrorIcon as={AlertCircleIcon} />
+              <FormControlErrorText>{emailInvalidText}</FormControlErrorText>
+            </FormControlError>
+          </FormControl>
         </Box>
         <Box>
           <Button
