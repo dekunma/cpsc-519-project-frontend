9a88480017addf2ee8a9f4f4b1d7ac8abd6dcab0
Duncan Du <duncan.du@yale.edu>
2024-04-05 21:24:16 -0400

    added feature to display images in pin/post details

--- screens/PinDetailScreen.jsx
+++ screens/PinDetailScreen.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import {View, Text, Button, StyleSheet, FlatList} from 'react-native';
+import { Image } from "@gluestack-ui/themed"
 import {useNavigation} from '@react-navigation/native';
 import api from "../api";
 
@@ -10,16 +11,28 @@ const PinDetailScreen = ({route}) => {
   const [images, setImages] = useState([]);
 
   console.debug("pinDetails = " + JSON.stringify(pinDetails))
-  // useEffect(() => {
-  //   api
-  //     .get('/posts/profile')
-  //     .then(response => {
-  //       setName(response.data.name);
-  //       setEmail(response.data.email);
-  //       setAvatar(response.data.avatar);
-  //     })
-  //     .catch(e => console.log(e));
-  // }, []);
+  useEffect(() => {
+    api.get(`/posts/get-post-images?post_id=${pinDetails.postId}`)
+      .then(response => {
+        console.debug("response.data['images'].length = " + response.data['images'].length);
+        if (response.data['images'].length > 0) {
+          const newImages = response.data['images'].map(image => image['file_path']);
+          setImages(newImages); // update the state
+          console.debug("images stored: " + JSON.stringify(images));
+        }
+        console.log("Response from get post images: " + JSON.stringify(response.data));
+      })
+      .catch(e => console.log(e));
+  }, [pinDetails.postId]);
+
+  const renderItem = ({ item }) => {
+    console.log("Rendering item with URL:", item); // Log the URL
+
+    return (
+      <Image source={{ uri: item }} />
+    );
+  };
+
 
   return (
     <View style={styles.container}>
@@ -29,6 +42,14 @@ const PinDetailScreen = ({route}) => {
       {/* Displaying latitude and longitude */}
       <Text>Latitude: {pinDetails.coordinate.latitude}</Text>
       <Text>Longitude: {pinDetails.coordinate.longitude}</Text>
+      {/* Display the images associated with this post */}
+      <FlatList
+        data={images}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index.toString()}
+        numColumns={2}
+        contentContainerStyle={styles.imageList}
+      />
       <Button title="go back" onPress={() => navigation.goBack()}/>
     </View>
   );
@@ -53,6 +74,10 @@ const styles = StyleSheet.create({
   postId: {
     fontSize: 14,
     marginBottom: 10,
+  },
+  imageList: {
+    flexGrow: 1,
+    justifyContent: 'center',
   },
 });
 
